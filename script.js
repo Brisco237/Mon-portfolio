fa80743ebdbb7c99f602e3b765c68fe8d5a79b9e
kamdem Brice <kamdembrice770@gmail.com>
2025-09-30 01:35:06 +0100

    add skills section

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
     new TypeIt("#element", {}).go();
 });
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("#aboutme", {
+    scrollTrigger: {
+      trigger: "#aboutme-photo",
+      start: "right 80%", // déclenche quand la box arrive à 80% de la fenêtre
+      toggleActions: "play none none reverse"
+    },
+    opacity: 1,
+    y: -50,
+    duration: 3
+});
+gsap.to("#home-section", {
+    scrollTrigger: {
+      trigger: "#home-section",
+      start: "bottom 50%", // déclenche quand la box arrive à 80% de la fenêtre
+      toggleActions: "play none none reverse"
+    },
+    opacity: 1,
+    y: -50,
+    duration: 3
+});
