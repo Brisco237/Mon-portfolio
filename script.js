e7b0a9558a56c85bb3e5f9530b7a5346b585857b
kamdem Brice <kamdembrice770@gmail.com>
2025-10-10 23:50:25 +0100

    add animation with animate on scroll(AOS)

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
     new TypeIt("#element", {}).go();
 });
-
-gsap.registerPlugin(ScrollTrigger);
-
-gsap.to('', {
-    scrollTrigger: {
-      trigger: "",
-      start: "right 80%", // déclenche quand la box arrive à 80% de la fenêtre
-      toggleActions: "play none none reverse"
-    },
-    opacity: 1,
-    y: -50,
-    duration: 3
-});
