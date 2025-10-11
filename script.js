f97d47a6276038d7514cda51780e9307b31981ce
kamdem Brice <kamdembrice770@gmail.com>
2025-10-11 15:34:39 +0100

    add new icon social network

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     new TypeIt("#element", {}).go();
 });
+// Bouton retour en haut
+let btnTop = document.getElementById("btnTop");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        btnTop.style.display = "block";
+    } else {
+        btnTop.style.display = "none";
+    }
+}
+function topFunction() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
